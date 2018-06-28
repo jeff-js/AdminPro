28d6644834ce265c739f0c4153455ad9fceb9241
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2018-06-27 21:57:08 -0500

    Implementación de Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//Rutas
+import { APP_ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
+import { NoPageFoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NoPageFoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SideBarComponent,
+    BreadCrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/nopagefound/nopagefound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-page-found',
+  templateUrl: './nopagefound.component.html',
+  styles: []
+})
+export class NoPageFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
